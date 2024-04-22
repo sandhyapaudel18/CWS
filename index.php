6989f4ff9d5e0ca20cde034fa8ceb9dd19b37b77
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-04-22 21:10:07 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        This is a php tutorial
+        <?php 
+        echo"Hello World";
+         //hello world
+         ?>
+    </div>
+   
+</body>
+</html>
