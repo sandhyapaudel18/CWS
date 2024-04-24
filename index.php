9677ec15fbd8c309424ce1e81b289282071097ec
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-04-24 10:33:34 +0545

    changes

--- index.php
+++ index.php
@@ -6,18 +6,27 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        This is a php tutorial
-        <?php 
-        echo"Hello World";
+    <? php
+    $name=$_POST["name"];
+    $gender=$_POST["gender"];
+    $address=$_POST["address"];
+    $email=$_POST["email"];
+    $country=$_POST["country"];
+    $subjects=$_POST["subjects"];
+    $bio=$_POST["bio"];
+    $password=$_POST["password"];
+    $dateofbirth=$_POST["dateofbirth"];
+    $conn=mysqli_connect("localhost","root","root123","");
+    $inser="INSERT INTO student($name,$gender,$address,$email,$country,$subjects,$bio,$password,$dateofbirth) VALUES('$name','$gender','$address','$email','$country','$subjects','$bio','$password','$dateofbirth')";
+    $run=mysqli_connect($conn,$inser);
+
+    if($run==true){
+        echo"Thank you for Submitting your Form:"
+    }
 
 
-         //hello world
-         $var=10;
-         $var2=15;
-         echo "The value of ";
-         ?>
-    </div>
+    ?>
+
    
 </body>
-</html>
+</html>                                                                                                                                                                                     
